a75c04666704359552bee3d802e784f04070961e
Jiati Le <timle1029@gmail.com>
2017-01-11 17:34:40 -0500

    update setting, remove credential

--- Rememeber/Rememeber/settings.py
+++ Rememeber/Rememeber/settings.py
@@ -152,7 +152,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.andrew.cmu.edu'
 EMAIL_HOST = 'smtp.gmail.com'
 #
-EMAIL_HOST_USER = 'luckyle1029@gmail.com'
+EMAIL_HOST_USER = ''
 #
 EMAIL_HOST_PASSWORD = ''
 
@@ -161,4 +161,3 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-# DEFAULT_FROM_EMAIL = 'jiatil@andrew.cmu.edu'
